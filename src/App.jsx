3299f0049ef2bc63e5d3e27adfc45a27ccf37721
Scott C. Reynolds <scott@scottcreynolds.com>
2023-02-10 16:12:05 -0500

    get a basic results grid going with react-masonry-css, fair warning, it is ugly

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { SearchInput } from './features/SearchInput'
 import { searchGifs } from './api/giphy'
+import Masonry from 'react-masonry-css'
+
+const breakpointCols = {
+  default: 3,
+  1100: 2,
+  700: 1
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [gifs, setGifs] = useState([])
 
   const handleSearch = async (term) => {
     const results = await searchGifs(term)
     console.log(results)
+    setGifs(results)
   }
 
   return (
     <div className="App">
-      <SearchInput handleSearch={handleSearch} />
+      <SearchInput handleSearch={handleSearch}/>
+      <Masonry breakpointCols={breakpointCols} className="results-grid" columnClassName='results-col'>
+        {gifs.map((gif) => (
+          <div><img src={gif.images.downsized.url} /></div>
+        ))}
+      </Masonry>
     </div>
   )
 }
