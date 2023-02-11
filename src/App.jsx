4bb3bb959b5f3e58c4c098b8efdeeb0ac6c3b2a0
Scott C. Reynolds <scott@scottcreynolds.com>
2023-02-10 20:09:50 -0500

    basic infinite scroll mechanism in place

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { searchGifs, getTrendingGifs } from './api/giphy'
 import { RecentSearches } from './features/RecentSearches'
 import { getRecentSearches, addRecentSearch } from './api/user'
 import Masonry from 'react-masonry-css'
+import useIntersectionObserver from '@react-hook/intersection-observer'
 
 const breakpointCols = {
   default: 3,
@@ -17,17 +18,20 @@ function App() {
   const [count, setCount] = useState(0)
   const [gifs, setGifs] = useState([])
   const [recentSearches, setRecentSearches] = useState([])
+  const [ref, setRef] = useState()
+  const {isIntersecting} = useIntersectionObserver(ref)
+  const [term, setTerm] = useState('')
+  const [searchType, setSearchType] = useState('trending')
 
   const handleSearch = async (term) => {
     const results = await searchGifs(term)
-    console.log(results)
     setGifs(results)
+    setTerm(term)
     setRecentSearches(await addRecentSearch(term))
   }
 
   const getTrending = async () => {
     const results = await getTrendingGifs()
-    console.log(results)
     setGifs(results)
   }
 
@@ -35,6 +39,21 @@ function App() {
     const results = getRecentSearches()
     setRecentSearches(results)
   }, [])
+
+  useEffect(() => {
+    const getNextPage = async () => {
+      let results = []
+      if(searchType === 'trending') {
+        results = await getTrendingGifs(gifs.length)
+      } else {
+        results = await searchGifs(term, gifs.length)
+      }
+      setGifs([...gifs, ...results])
+    }
+    if(isIntersecting) {
+      getNextPage()
+    }
+  }, [isIntersecting])
 
   return (
     <div className="App">
@@ -46,6 +65,7 @@ function App() {
           <div><img src={gif.images.downsized.url} /></div>
         ))}
       </Masonry>
+      {gifs.length > 0 && <hr ref={setRef} />}
     </div>
   )
 }
--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,16 +1,16 @@
 const KEY = import.meta.env.VITE_GIPHY_API_KEY
 
-export const searchGifs = async (query) => {
+export const searchGifs = async (query, offset = 0) => {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    `https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${query}&limit=25&offset=${offset}&rating=g&lang=en`
   )
   const { data } = await response.json()
   return data
 }
 
-export const getTrendingGifs = async () => {
+export const getTrendingGifs = async (offset = 0) => {
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/trending?api_key=${KEY}&limit=25&rating=g`
+    `https://api.giphy.com/v1/gifs/trending?api_key=${KEY}&limit=25&rating=g&offset=${offset}`
   )
   const { data } = await response.json()
   return data
